ece9ee2865076211d48484e87e69a1907755f321
Kostaintyn Kikinov <kikinov.k@gmail.com>
2024-01-25 23:02:00 +0200

    feat: add implementation rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,22 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { getDirNameFromUrl } from '../utils/getDirNameFromUrl.js';
+
 const rename = async () => {
-    // Write your code here 
+    const sourceDir = './files';
+    const sourceFile = 'wrongFilename.txt';
+    const targetFile = 'wrongFilename.md';
+    const errorMessage = 'FS operation failed';
+
+    const currentDirPath = getDirNameFromUrl(import.meta.url);
+    const sourceFilePath = path.resolve(currentDirPath, sourceDir, sourceFile);
+    const targetFilePath = path.resolve(currentDirPath, sourceDir, targetFile);
+
+    try {
+        await fs.rename(sourceFilePath, targetFilePath);
+    } catch {
+        throw Error(errorMessage);
+    }
 };
 
 await rename();
