5320edeae96129f5b107e42fce6525615354066b
Kostaintyn Kikinov <kikinov.k@gmail.com>
2024-01-25 23:22:55 +0200

    feat: add implementation delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { getDirNameFromUrl } from '../utils/getDirNameFromUrl.js';
+
 const remove = async () => {
-    // Write your code here 
-};
+    const sourceDir = './files';
+    const fileName = 'fileToRemove.txt';
+    const errorMessage = 'FS operation failed';
+
+    const currentDirPath = getDirNameFromUrl(import.meta.url);
+    const pathName = path.join(currentDirPath, sourceDir, fileName);
+
+    try {
+        await fs.unlink(pathName);
+    } catch {
+        throw Error(errorMessage);
+    }
+}
 
 await remove();
