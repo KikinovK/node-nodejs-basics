b9878f30a702ff86a4a12d39420a1d06f27b5d83
Kostaintyn Kikinov <kikinov.k@gmail.com>
2024-01-25 23:38:57 +0200

    feat: add implementation read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,23 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { getDirNameFromUrl } from '../utils/getDirNameFromUrl.js';
+
 const read = async () => {
-    // Write your code here 
+    const sourceDir = './files';
+    const fileName = 'fileToRead.txt';
+    const errorMessage = 'FS operation failed';
+
+    const currentDirPath = getDirNameFromUrl(import.meta.url);
+    const pathName = path.join(currentDirPath, sourceDir, fileName);
+
+    try {
+        const fileContent = await fs.readFile(pathName, 'utf-8');
+
+        console.log(`Content of ${pathName}:`);
+        console.log(fileContent);
+    } catch {
+        throw Error(errorMessage);
+    }
 };
 
 await read();
