a9743b18712a102bbc61ffc5ecc3e8396d67a8c0
Kostaintyn Kikinov <kikinov.k@gmail.com>
2024-01-27 22:14:12 +0200

    feat: add implemention write of stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+import { getDirNameFromUrl } from '../utils/getDirNameFromUrl.js';
+
 const write = async () => {
-    // Write your code here 
+    const sourceDir = './files';
+    const fileName = 'fileToWrite.txt';
+
+    const currentDirPath = getDirNameFromUrl(import.meta.url);
+    const filePath = path.join(currentDirPath, sourceDir, fileName);
+
+    const fileStream = fs.createWriteStream(filePath, { encoding: 'utf-8' });
+
+    process.stdin.pipe(fileStream);
+
+    fileStream.on('error', (error) => {
+        console.error('Error writing to file:', error);
+    });
+
+    console.log('Enter your details (press Ctrl + D to complete entry):');
 };
 
 await write();
