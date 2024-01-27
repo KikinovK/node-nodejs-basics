7f7b3495c26e3e3f0798a380c9ff447dfeb84d43
Kostaintyn Kikinov <kikinov.k@gmail.com>
2024-01-27 22:01:45 +0200

    feat: add implemention read to stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,30 @@
+import fs from 'fs';
+import path from 'path';
+import { getDirNameFromUrl } from '../utils/getDirNameFromUrl.js';
+
 const read = async () => {
-    // Write your code here 
+    const sourceDir = './files';
+    const fileName = 'fileToRead.txt';
+
+    const currentDirPath = getDirNameFromUrl(import.meta.url);
+    const filePath = path.join(currentDirPath, sourceDir, fileName);
+
+    const fileStream = fs.createReadStream(filePath, { encoding: 'utf-8' });
+    const consoleStream = process.stdout;
+
+    consoleStream.write(`File of ${fileName}:\n`);
+
+    fileStream.on('data', (chunk) => {
+        consoleStream.write(chunk);
+    });
+
+    fileStream.on('end', () => {
+        consoleStream.write('\n');
+    });
+
+    fileStream.on('error', (error) => {
+        console.error('Error reading file:', error);
+    });
 };
 
 await read();
