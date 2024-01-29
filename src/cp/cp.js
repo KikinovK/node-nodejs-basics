78bc2a63c826916dc3564d9106ba72e70c0fac6f
Kostaintyn Kikinov <kikinov.k@gmail.com>
2024-01-29 16:57:02 +0200

    feat: add implemention child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,27 @@
+import { spawn } from 'child_process';
+import path from 'path';
+import { getDirNameFromUrl } from '../utils/getDirNameFromUrl.js';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const sourceDir = './files';
+    const processFileName = 'script.js';
+
+    const currentDirPath = getDirNameFromUrl(import.meta.url);
+    const processFilePath = path.join(currentDirPath, sourceDir, processFileName);
+
+    const childProcess = spawn('node', [processFilePath, ...args], { stdio: ['pipe', 'pipe', 'inherit'] });
+
+    process.stdin.pipe(childProcess.stdin);
+    childProcess.stdout.pipe(process.stdout);
+
+    childProcess.on('close', (code) => {
+        console.log(`Child process exited with code ${code}`);
+    });
+
+    childProcess.on('error', (err) => {
+        console.error('Error spawning child process:', err);
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ['arg1', 'arg2', 'arg3']);
