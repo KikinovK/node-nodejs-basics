5c1323c07983cbb7f9d4f36bcd3f041c8311fe53
Kostaintyn Kikinov <kikinov.k@gmail.com>
2024-10-04 18:00:04 +0300

    fix: add error handler to rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -5,7 +5,7 @@ import { getDirNameFromUrl } from '../utils/getDirNameFromUrl.js';
 const rename = async () => {
     const sourceDir = './files';
     const sourceFile = 'wrongFilename.txt';
-    const targetFile = 'wrongFilename.md';
+    const targetFile = 'properFilename.md';
     const errorMessage = 'FS operation failed';
 
     const currentDirPath = getDirNameFromUrl(import.meta.url);
@@ -13,9 +13,17 @@ const rename = async () => {
     const targetFilePath = path.resolve(currentDirPath, sourceDir, targetFile);
 
     try {
-        await fs.rename(sourceFilePath, targetFilePath);
-    } catch {
-        throw Error(errorMessage);
+        try {
+            await fs.access(targetFilePath);
+            throw new Error(`Target file '${targetFile}' already exists.`);
+        } catch(error) {
+            if (error.code !== 'ENOENT') {
+                throw new Error(error.message);
+            }
+            await fs.rename(sourceFilePath, targetFilePath);
+        }
+    } catch(error) {
+        throw new Error(`${errorMessage}: ${error.message}`);
     }
 };
 
