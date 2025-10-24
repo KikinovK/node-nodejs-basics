2dc1c692e4db76a0342a592a340f95c34d113bce
Kostaintyn Kikinov <kikinov.k@gmail.com>
2025-10-24 19:29:29 +0300

    feat: add cli:env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-  // Write your code here
+  const prefix = 'RSS_';
+  const separator = '; ';
+
+  const rssVars = Object.entries(process.env)
+    .filter(([key]) => key.startsWith(prefix))
+    .map(([key, value]) => `${key}=${value}`)
+    .join(separator);
+
+  console.log(rssVars);
 };
 
 parseEnv();
