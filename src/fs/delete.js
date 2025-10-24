3c137e9eb4edbc057d53b8d808b45c3bf9b4b5ae
Kostaintyn Kikinov <kikinov.k@gmail.com>
2025-10-24 19:03:13 +0300

    feat: add fs:delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,21 @@
+import { promises as fs } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const remove = async () => {
-  // Write your code here
+  const filesDir = 'files';
+  const fileName = 'fileToRemove.txt';
+  const errorMessage = 'FS operation failed';
+
+  const filePath = join(__dirname, filesDir, fileName);
+
+  try {
+    await fs.unlink(filePath);
+  } catch (error) {
+    throw new Error(`${errorMessage}: ${error.message}`);
+  }
 };
 
 await remove();
