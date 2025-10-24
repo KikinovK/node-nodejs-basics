6615d0233b59a6dbc7634c3e5afa71360d28f378
Kostaintyn Kikinov <kikinov.k@gmail.com>
2025-10-24 19:36:46 +0300

    feat: add cli:args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,20 @@
 const parseArgs = () => {
-  // Write your code here
+  const argPrefix = '--';
+  const separator = ', ';
+  const connector = ' is ';
+
+  const args = process.argv.slice(2);
+  const result = [];
+
+  for (let i = 0; i < args.length; i += 2) {
+    if (args[i].startsWith(argPrefix)) {
+      const propName = args[i].slice(2);
+      const value = args[i + 1];
+      result.push(`${propName}${connector}${value}`);
+    }
+  }
+
+  console.log(result.join(separator));
 };
 
 parseArgs();
