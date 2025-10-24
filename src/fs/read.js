eaa2883409902b45c658f3edde8b5a4d6e78b7fd
Kostaintyn Kikinov <kikinov.k@gmail.com>
2025-10-24 19:14:48 +0300

    feat: add fs:read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,24 @@
+import { promises as fs } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const read = async () => {
-  // Write your code here
+  const filesDir = 'files';
+  const fileName = 'fileToRead.txt';
+  const errorMessage = 'FS operation failed';
+  const encoding = 'utf8';
+
+  const filePath = join(__dirname, filesDir, fileName);
+
+  try {
+    const content = await fs.readFile(filePath, encoding);
+    console.log(`Content of ${filePath}:`);
+    console.log(content);
+  } catch (error) {
+    throw new Error(`${errorMessage}: ${error.message}`);
+  }
 };
 
 await read();
