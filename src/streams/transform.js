da63ef5a96756fae7afc7dcfcc6d857b8db9abc7
Kostaintyn Kikinov <kikinov.k@gmail.com>
2025-10-25 14:14:59 +0300

    feat: add streams:transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,22 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-  // Write your code here
+  const promptMessage = 'Enter text to transform: ';
+  const resultMessage = 'Transformed text: ';
+  const exitMessage = '(press Ctrl + C to exit)\n\n';
+
+  const transformStream = new Transform({
+    transform(chunk, encoding, callback) {
+      const original = chunk.toString();
+
+      const text = original.replace(/[\x00-\x1F\x7F]/g, '');
+      const transformed = text.split('').reverse().join('') + '\n';
+      callback(null, resultMessage + transformed + exitMessage + promptMessage);
+    }
+  });
+
+  process.stdout.write(promptMessage);
+  process.stdin.pipe(transformStream).pipe(process.stdout);
 };
 
 await transform();
